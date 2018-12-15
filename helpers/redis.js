dc3ba94f8f58ebec71773c850cdb4be429058164
Hüseyin TERKİR <hsynterkr@gmail.com>
2018-12-15 23:53:01 +0300

    Update redis.js

--- helpers/redis.js
+++ helpers/redis.js
@@ -3,6 +3,10 @@ const bluebird = require('bluebird');
 
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
-const client = redis.createClient(process.env.REDISCLOUD_URL);
+const client = redis.createClient({
+  port      : 19251,               // replace with your port
+  host      : 'redis-19251.c11.us-east-1-2.ec2.cloud.redislabs.com',        // replace with your hostanme or IP address
+  password  : 'bnjLiMHGuW9S629JAHyoKV0G0EXPbRzi',    // replace with your password
+});
 
 module.exports = client;
